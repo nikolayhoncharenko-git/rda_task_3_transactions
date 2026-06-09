157a50b32384dbc635c2d1a7a576822939128c39
Mykola Honcharenko <nikolay.honcharenko#gmail.com>
2026-06-09 23:52:21 +0300

    remove block count

--- task.sql
+++ task.sql
@@ -5,8 +5,8 @@ START TRANSACTION;
 INSERT INTO ShopDB.Orders (CustomerID, Date)
 	VALUES (1, '2023-01-01');
 
-INSERT INTO ShopDB.OrderItems(OrderID, ProductID, Count)
-    VALUES (LAST_INSERT_ID(), 1, 1);
+INSERT INTO ShopDB.OrderItems(OrderID, ProductID)
+    VALUES (LAST_INSERT_ID(), 1);
 
 UPDATE ShopDB.Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 
