021ac45f1355d048b842fab1f8e62badf5cb014d
Mykola Honcharenko <nikolay.honcharenko#gmail.com>
2026-06-09 23:37:53 +0300

    Змін блоку для таблиці OrderItems.

--- task.sql
+++ task.sql
@@ -5,9 +5,9 @@ START TRANSACTION;
 INSERT INTO ShopDB.Orders (CustomerID, Date)
 	VALUES (1, '2023-01-01');
 
-INSERT INTO ShopDB.OrderItems(ProductID, Count)
-    VALUES (LAST_INSERT_ID(), 1);
+INSERT INTO ShopDB.OrderItems(OrderID, ProductID, Count)
+    VALUES (LAST_INSERT_ID(), 1, 1);
 
 UPDATE ShopDB.Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 
-COMMIT; 
+COMMIT;
