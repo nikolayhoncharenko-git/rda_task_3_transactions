fe069889bdbd71772accade77dbf2767ee81d608
Mykola Honcharenko <nikolay.honcharenko#gmail.com>
2026-06-10 00:39:40 +0300

    Prefixes and the date in brackets have been removed

--- task.sql
+++ task.sql
@@ -1,8 +1,8 @@
 USE ShopDB;
 START TRANSACTION;
-INSERT INTO ShopDB.Orders (CustomerID, Date)
+INSERT INTO Orders (CustomerID, `Date`)
 VALUES (1, '2023-01-01');
-INSERT INTO ShopDB.OrderItems(OrderID, ProductID, Count)
+INSERT INTO OrderItems (OrderID, ProductID, Count)
 VALUES (LAST_INSERT_ID(), 1, 1);
-UPDATE ShopDB.Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 COMMIT;
