75ff657ad2dfdabb181358a3381cca87bb2a93b7
Mykola Honcharenko <nikolay.honcharenko#gmail.com>
2026-06-10 19:35:49 +0300

    Remove Count in OrderItems

--- task.sql
+++ task.sql
@@ -2,7 +2,7 @@ USE ShopDB;
 START TRANSACTION;
 INSERT INTO Orders (CustomerID, `Date`)
 VALUES (1, '2023-01-01');
-INSERT INTO OrderItems (OrderID, ProductID, `Count`)
-VALUES (LAST_INSERT_ID(), 1, 1);
+INSERT INTO OrderItems (OrderID, ProductID)
+VALUES (LAST_INSERT_ID(), 1);
 UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = 1;
 COMMIT;
